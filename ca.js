b70ddc28b78ffa8cea6a4c63c3ee19e2d431cc7d
dharshanparamasivam-tech <dharshanparamasivam@gmail.com>
2026-02-25 22:41:40 +0530

    Create ca.js

---
+++ ca.js
@@ -0,0 +1,20 @@
+void setup() {
+  pinMode(3, OUTPUT);
+    digitalWrite(3, HIGH);
+    delay(1000);
+    digitalWrite(3, LOW);
+    delay(1000);
+    pinMode(3, OUTPUT);
+    digitalWrite(3, HIGH);
+    delay(1000);
+    digitalWrite(3, LOW);
+    delay(1000);
+    pinMode(3, OUTPUT);
+    digitalWrite(3, HIGH);
+    delay(1000);
+    digitalWrite(3, LOW);
+    delay(1000);
+}
+
+void loop() {
+}
